b1b89181d71bf5d9520f72f55f002708e90c57f5
Vekhov Vadim <106478770+wizardVadim@users.noreply.github.com>
2026-09-03 00:33:30 +0800

    feat(redis): add client and integration test infrastructure (#24)

---
+++ internal/features/matchmaking/repository/redis.go
@@ -0,0 +1,15 @@
+package repository
+
+import (
+	"github.com/redis/go-redis/v9"
+)
+
+type RedisRepository struct {
+	client *redis.Client
+}
+
+func NewRedisRepository(client *redis.Client) *RedisRepository {
+	return &RedisRepository{
+		client: client,
+	}
+}
